cfc6fb762833a57f9439bf1fdf0ccb933d784eda
omar <ogc@gerardo.com>
2018-06-20 11:40:13 -0500

    correcion error

--- src/holamundo/holamundo.java
+++ src/holamundo/holamundo.java
@@ -3,7 +3,8 @@ package holamundo;
 public class holamundo {
 	public static void main(String[] args) {		
 		System.out.println("Hola Mundo");
-		System.out.println("como estas")
+		System.out.println("como estas");
+		System.out.println("todo bien");
 	}
 
 }
